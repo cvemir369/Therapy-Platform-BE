4eb9bc959d9a832f78e0a2dfbe7b507a7c48576a
cvemir369 <cvemir369@gmail.com>
2025-02-25 14:05:54 +0100

    added chatters api endpoint to get ids of patients that contacted therapist

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -3,6 +3,7 @@ import {
   getMessages,
   createMessage,
   markAsRead,
+  getChatters,
 } from "../controllers/messageController.js";
 // import isAuthorized from "../middlewares/isAuthorized.js";
 
@@ -13,5 +14,8 @@ messageRouter.route("/").get(getMessages).post(createMessage);
 
 // mark message as read
 messageRouter.route("/:id").patch(markAsRead);
+
+// get all chatters
+messageRouter.route("/chatters").get(getChatters);
 
 export default messageRouter;
